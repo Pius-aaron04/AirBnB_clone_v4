2d1be5fd4b8f9dfcedc2f2088e5639c904ac017c
Aaron Pius <piuschbz@gmail.com>
2024-02-10 16:34:57 +0000

    Task 4 and 5 finishing

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -4,51 +4,51 @@
  */
 
 $(document).ready(function () {
-    const selectedAmenities = [];
-  
-    // Listen for changes in the input checkbox
-    $('input[type="checkbox"]').change(function () {
-      const amenityId = $(this).data('name');
-  
-      if ($(this).is(':checked')) {
-        selectedAmenities.push(amenityId);
-      } else {
-        const index = selectedAmenities.indexOf(amenityId);
-        if (index !== -1) {
-          selectedAmenities.splice(index, 1);
-        }
+  const selectedAmenities = [];
+
+  // Listen for changes in the input checkbox
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities.push(amenityId);
+    } else {
+      const index = selectedAmenities.indexOf(amenityId);
+      if (index !== -1) {
+        selectedAmenities.splice(index, 1);
       }
-      const amenitiesText = selectedAmenities.join(', ');
-      $('div.amenities h4').text(amenitiesText);
-    });
-  
-    // Make a request to get places data
-    $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/places_search/',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({}), // Send an empty dictionary in the body
-      success: function (data) {
-        // Clear existing places
-        $('section.places').empty();
-  
-        // Loop through the result and create HTML elements for each place
+    }
+    const amenitiesText = selectedAmenities.join(', ');
+    $('div.amenities h4').text(amenitiesText);
+  });
+
+  // Make a request to get places data
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}), // Send an empty dictionary in the body
+    success: function (data) {
+      // Clear existing places
+      $('section.places').empty();
+
+      // Loop through the result and create HTML elements for each place
         data.forEach(function (place) {
-          const article = $('<article>').append(
-            $('<div>').append(
-              $('<div>').append(
-                $('<h2>').text(place.name),
-                $('<div>').addClass('price_by_night').text('$' + place.price_by_night)
-              ),
-              $('<div>').addClass('information').append(
-                $('<div>').addClass('max_guest').text(place.max_guest + ' Guest(s)'),
-                $('<div>').addClass('number_rooms').text(place.number_rooms + ' Bedroom(s)'),
-                $('<div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathroom(s)')
-              )
+          const article = $('<article>').addClass('place'); // Unique class for each element
+
+          article.append(
+            $('<div>').addClass('title_box').append(
+              $('<h2>').text(place.name),
+              $('<div>').addClass('price_by_night').text('$' + place.price_by_night)
             ),
-            $('<div>').addClass('description').text(place.description)
+            $('<div>').addClass('information').append(
+              $('<div>').addClass('max_guest').text(place.max_guest + ' Guest(s)'),
+              $('<div>').addClass('number_rooms').text(place.number_rooms + ' Bedroom(s)'),
+              $('<div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathroom(s)')
+            ),
+            $('<div>').addClass('description').html(place.description)
           );
-  
+
           // Append the created article to the places section
           $('section.places').append(article);
         });
@@ -56,15 +56,14 @@ $(document).ready(function () {
       error: function (xhr, status, error) {
         console.error('Error:', error);
       }
-    });
-  
-    // Check API-status
-    $.get('http://localhost:5001/api/v1/status/', function (data) {
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
-    });
   });
-  
+
+  // Check API-status
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
